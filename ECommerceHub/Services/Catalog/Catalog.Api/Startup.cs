16ff846913b44a4fb348e941a4ac7c63deaecf1e
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-08-03 01:10:20 +0300

    add startup class  and configure Program class for sepiration

---
+++ ECommerceHub/Services/Catalog/Catalog.Api/Startup.cs
@@ -0,0 +1,41 @@
+﻿namespace Catalog.Api
+{
+    public class Startup
+    {
+        public IConfiguration Configuration;
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+
+                app.UseDeveloperExceptionPage();
+
+            }
+
+            app.UseRouting();
+            app.UseStaticFiles();
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+
+
+        }
+
+
+    }
+}
