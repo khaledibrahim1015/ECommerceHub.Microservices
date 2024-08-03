0d461c8b0f94da7e6a8383978048210f493d74a4
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-08-03 12:16:52 +0300

    impl BaseController with ApiVersioning

---
+++ ECommerceHub/Services/Catalog/Catalog.Api/Controllers/ApiController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Api.Controllers;
+[ApiVersion("1")]
+[Route("api/v{version:apiVersion}/[controller]")]
+[ApiController]
+public class ApiController :ControllerBase
+{
+}
