8fc57f1a99b6f456e6c239b4a07184ae6d8adeed
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-08-07 08:27:54 +0300

    impelement BasketEndpoints

---
+++ ECommerceHub/Services/Basket/Basket.Api/Controllers/ApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Basket.Api.Controllers;
+
+[ApiVersion("1")]
+[Route("api/v{version:apiVersion}/[contrller]")]
+[ApiController]
+public class ApiController :ControllerBase
+{
+}
---
+++ ECommerceHub/Services/Basket/Basket.Api/Controllers/BasketController.cs
@@ -0,0 +1,56 @@
+﻿using Basket.Application.Commands;
+using Basket.Application.Queries;
+using Basket.Application.Responses;
+using Basket.Core.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Basket.Api.Controllers;
+
+public class BasketController:ApiController
+{
+    private readonly IMediator _mediator;
+
+    public BasketController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    [Route("[action]/{userName}", Name ="GetBasketByUserName")]
+    [ProducesResponseType(typeof(ShoppingCartResponse), (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<ShoppingCartResponse>> GetBasket(string userName)
+    {
+        var query = new GetBasketByUserNameQuery(userName);
+        var shoppingCartResponse = await  _mediator.Send(query);
+        return Ok(shoppingCartResponse);
+    }
+
+    [HttpPost]
+    [Route("CreateBasket")]
+    [ProducesResponseType(typeof(ShoppingCartResponse) , (int)HttpStatusCode.OK)]
+    public async Task<ActionResult<ShoppingCartResponse>> UpdateBasket
+        ( [FromBody] CreateShoppingCartCommand createShoppingCartCommand )
+    {
+
+        var basket=  _mediator.Send(createShoppingCartCommand);
+        return Ok(basket);
+
+    }
+
+    [HttpDelete]
+    [Route("[action]/{userName}", Name ="DeleteBasketByUserName")]
+    [ProducesResponseType((int)HttpStatusCode.OK)]
+    public async Task<IActionResult> DeleteBasket(string userName)
+    {
+        var command =  new DeleteBasketByUserNameCommand(userName);
+        await _mediator.Send(command);
+        return Ok();
+    }
+
+
+
+
+
+}
