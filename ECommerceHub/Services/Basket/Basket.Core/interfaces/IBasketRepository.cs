c707e6bc3590b6ae6ed5017947354d9c5ddc6d5d
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-08-07 03:37:40 +0300

    add base impl for Basket.Core

---
+++ ECommerceHub/Services/Basket/Basket.Core/interfaces/IBasketRepository.cs
@@ -0,0 +1,11 @@
+﻿using Basket.Core.Entities;
+
+namespace Basket.Core.interfaces;
+
+public interface IBasketRepository
+{
+    Task<ShoppingCart> GetBasket(string userName);
+    Task<ShoppingCart> UpdateBasket(ShoppingCart cart);
+    Task DeleteBasket (string userName);
+
+}
