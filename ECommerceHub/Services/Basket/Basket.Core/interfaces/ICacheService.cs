ac4d5850f63f38747e30d96000ad5ce70cccf4de
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-08-07 04:54:19 +0300

    impl IcacheService

--- ECommerceHub/Services/Basket/Basket.Core/interfaces/ICacheService.cs
+++ ECommerceHub/Services/Basket/Basket.Core/interfaces/ICacheService.cs
@@ -1,9 +1,9 @@
-﻿namespace Basket.Core.Repositories;
+﻿namespace Basket.Core.interfaces;
 
-internal interface ICacheService
+public interface ICacheService
 {
-    T GetData<T>(string key);
-    bool SetData<T>(string key , T value , DateTimeOffset expirationTime);
+    Task<T> GetDataAsync<T>(string key);
+    Task< bool> SetDataAsync<T>(string key , T value , DateTimeOffset expirationTime);
     object RemoveData(string key);
 
 }
---
+++ ECommerceHub/Services/Basket/Basket.Infrastructure/Services/CacheService.cs
@@ -0,0 +1,46 @@
+﻿using Basket.Core.interfaces;
+using Basket.Infrastructure.Helper;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+
+namespace Basket.Infrastructure.Services;
+
+public class CacheService : ICacheService
+{
+    private readonly IDatabase _db;
+
+    public CacheService()
+    {
+        _db  =  ConnectionHelper.Connection.GetDatabase();
+    }
+
+    public async Task< T?> GetDataAsync<T>(string key)
+    {
+        var value = await _db.StringGetAsync(key);
+        if (!string.IsNullOrEmpty(value))
+             return JsonConvert.DeserializeObject<T>(value);
+
+        return default; 
+    }
+
+
+    public async Task<bool> SetDataAsync<T>(string key, T value, DateTimeOffset expirationTime)
+    {
+        TimeSpan expiryTime = expirationTime.DateTime.Subtract(DateTime.Now);
+
+        bool isSet = await _db.StringSetAsync(key, JsonConvert.SerializeObject(value), expiryTime);
+        return isSet;
+
+    }
+    public object RemoveData(string key)
+    {
+        bool _isKeyExist = _db.KeyExists(key);
+        if (_isKeyExist == true)
+        {
+            return _db.KeyDelete(key);
+        }
+        return false;
+
+    }
+
+}
