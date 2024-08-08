24cb23eb59065d2f42890600837b409ea31b1e87
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-08-08 08:44:34 +0300

    adding requirerd services N DI Conatiner ServicecCollectionExtensions

---
+++ ECommerceHub/Services/Discount/Discount.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Discount.Core.Repositories;
+using Discount.Infrastructure.Configuration;
+using Discount.Infrastructure.Repositories;
+
+namespace Discount.Api.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+
+        public static ServiceCollection AddRequiredServices(this ServiceCollection services , IConfiguration configuration)
+        {
+            // configure Postgresql configuration 
+            services.Configure<PostgresqlConfiguration>(configuration.GetSection("DatabaseSettings"));
+
+            // Register services in DI container 
+            services.AddSingleton<AppSettings>();
+            services.AddSingleton<ConnectionManager>();
+
+            services.AddScoped<IDiscountRepository, DiscountRepostiory>();
+
+            return services;
+        }
+
+
+    }
+}
