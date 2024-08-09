3692c2a8e9ed5edb96a0b0c22cd5fdfdb0eae507
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-08-09 12:04:06 +0300

    impl Dynamic Migration Builder EntityMappr , ConectionManager and SchemaManager to Migrate db when APP build => inject in hostedService

--- ECommerceHub/Services/Discount/Discount.Api/Extensions/ServiceCollectionExtensions.cs
+++ ECommerceHub/Services/Discount/Discount.Api/Extensions/ServiceCollectionExtensions.cs
@@ -1,13 +1,15 @@
-﻿using Discount.Core.Repositories;
+﻿using Discount.Core.Entities;
+using Discount.Core.Repositories;
 using Discount.Infrastructure.Configuration;
+using Discount.Infrastructure.Configuration.DatabaseConfigurationManager;
 using Discount.Infrastructure.Repositories;
-
+using Discount.Infrastructure.Services;
 namespace Discount.Api.Extensions
 {
     public static class ServiceCollectionExtensions
     {
 
-        public static ServiceCollection AddRequiredServices(this ServiceCollection services , IConfiguration configuration)
+        public static IServiceCollection AddRequiredServices(this IServiceCollection services , IConfiguration configuration)
         {
             // configure Postgresql configuration 
             services.Configure<PostgresqlConfiguration>(configuration.GetSection("DatabaseSettings"));
@@ -15,6 +17,14 @@ namespace Discount.Api.Extensions
             // Register services in DI container 
             services.AddSingleton<AppSettings>();
             services.AddSingleton<ConnectionManager>();
+            services.AddSingleton<SchemaManager>();
+
+            services.AddSingleton<IEnumerable<Type>>(sp => new List<Type>
+            {
+                typeof(Coupon),
+            });
+
+            services.AddHostedService<DataBaseMigrationService>();
 
             services.AddScoped<IDiscountRepository, DiscountRepostiory>();
 
