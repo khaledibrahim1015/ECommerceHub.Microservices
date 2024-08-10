a0609c3c71b204f30a4ae6f30fdedb23a8326b6a
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-08-10 19:47:53 +0300

    override SaveChangeAsync()

--- ECommerceHub/Services/Ordering/Ordering.Core/Entities/BaseEntity.cs
+++ ECommerceHub/Services/Ordering/Ordering.Core/Entities/BaseEntity.cs
@@ -3,10 +3,10 @@
 public abstract  class BaseEntity
 {
     public int Id { get; protected set; }
-    public string? CreatedBy  { get; protected set; }
-    public DateTime? CreatedDate { get; protected set; }
-    public string?  LastModifiedBy { get; protected set; }
-    public DateTime? LastModifiedDate { get; protected set; }
+    public string? CreatedBy  { get;  set; }
+    public DateTime? CreatedDate { get;  set; }
+    public string?  LastModifiedBy { get;  set; }
+    public DateTime? LastModifiedDate { get;  set; }
 
 
 }
--- ECommerceHub/Services/Ordering/Ordering.Infrastructure/Data/OrderDbContext.cs
+++ ECommerceHub/Services/Ordering/Ordering.Infrastructure/Data/OrderDbContext.cs
@@ -9,7 +9,28 @@ public  class OrderDbContext :DbContext
     {
         
     }
-    public DbSet<Order> Orders => Set<Order>();
 
+    public DbSet<Order> Orders { get; set; }
 
+ 
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken =default)
+    {
+        foreach (var entry in ChangeTracker.Entries<BaseEntity>())
+        {
+            switch (entry.State)
+            {
+                case EntityState.Added:
+                    entry.Entity.CreatedDate = DateTime.Now;
+                    entry.Entity.CreatedBy = "khaled"; //TODO: This will be replaced Identity Server
+                    break;
+                case EntityState.Modified:
+                    entry.Entity.LastModifiedDate = DateTime.Now;
+                    entry.Entity.LastModifiedBy = "khaled"; //TODO: This will be replaced Identity Server
+                    break;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
 }
