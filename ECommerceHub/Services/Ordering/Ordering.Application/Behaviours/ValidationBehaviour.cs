43097fee225762137bcff6004d692ba62ea1c565
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-08-12 03:55:33 +0300

    create FluentValidation and ValidationBehaviour

---
+++ ECommerceHub/Services/Ordering/Ordering.Application/Behaviours/ValidationBehaviour.cs
@@ -0,0 +1,34 @@
+﻿using FluentValidation;
+using MediatR;
+using ValidationException = FluentValidation.ValidationException;
+namespace Ordering.Application.Behaviours;
+
+public class ValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+{
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehaviour(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (_validators.Any())
+        {
+            var context = new ValidationContext<TRequest>(request);
+            //This runs all the validation rules one by one returns the validation result
+            var validationResults = await Task.WhenAll(
+                _validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+            //Now, need to check for any failures
+            var failures = validationResults.SelectMany(vr => vr.Errors).Where(f => f != null).ToList();
+
+            if (failures.Count != 0)
+                throw new ValidationException(errors: failures);
+
+        }
+        return await next();
+
+
+    }
+}
---
+++ ECommerceHub/Services/Ordering/Ordering.Application/Validators/CheckoutOrderCommandValidator.cs
@@ -0,0 +1,42 @@
+﻿using FluentValidation;
+using Ordering.Application.Commands;
+
+namespace Ordering.Application.Validators;
+
+public  class CheckoutOrderCommandValidator :AbstractValidator<CheckoutOrderCommand>
+{
+    public CheckoutOrderCommandValidator()
+    {
+        RuleFor(o => o.UserName)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("{UserName} is Required")
+            .MaximumLength(100)
+            .WithMessage("{UserName} must not exceed 70 characters");
+
+
+        RuleFor(o => o.TotalPrice)
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("{TotalPrice} is required")
+            .GreaterThan(-1)
+            .WithMessage("{TotalPrice} should not be negative ");
+
+
+        RuleFor(o => o.EmailAddress)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("{EmailAddress} is required");
+
+
+         RuleFor(o => o.FirstName)
+         .NotEmpty()
+         .NotNull()
+         .WithMessage("{FirstName} is required");
+
+        RuleFor(o => o.LastName)
+            .NotEmpty()
+            .NotNull()
+            .WithMessage("{LastName} is required");
+    }
+}
