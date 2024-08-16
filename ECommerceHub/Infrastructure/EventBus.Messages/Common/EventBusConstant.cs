a933fa7a8f83aa5d2b9e0a5ccfed6ebc2398c2db
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-08-16 17:16:50 +0300

    Impelement EventBus Messages struture

---
+++ ECommerceHub/Infrastructure/EventBus.Messages/Common/EventBusConstant.cs
@@ -0,0 +1,7 @@
+﻿namespace EventBus.Messages.Common;
+
+public class EventBusConstant
+{
+    public const string BasketOrderExchange = "basket-order-Basket-exchange";
+    public const string BasketCheckoutQueue = "basketcheckout-queue";
+}
---
+++ ECommerceHub/Infrastructure/EventBus.Messages/Events/BaseIntegrationEvent.cs
@@ -0,0 +1,18 @@
+﻿namespace EventBus.Messages.Events;
+
+public class BaseIntegrationEvent
+{
+    public string Id { get; private set; }
+    public DateTime CreationDate { get; private set; }
+    public BaseIntegrationEvent()
+    {
+        Id = Guid.NewGuid().ToString();
+        CreationDate = DateTime.UtcNow;
+    }
+
+    public BaseIntegrationEvent(string id, DateTime creationDate)
+    {
+        Id = id;
+        CreationDate = creationDate;
+    }
+}
