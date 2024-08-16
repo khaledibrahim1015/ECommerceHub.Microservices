a0bb33863d104b1963f1ccf1c2b803a4d29195dc
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-08-16 18:50:14 +0300

    impelement OcelotApiGateway

---
+++ ECommerceHub/ApiGateways/Ocelot.Api/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Ocelot.Api;
+
+public class Program
+{
+
+
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    private static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+        .ConfigureAppConfiguration((hostBuilderCtx, configBuilder) =>
+        {
+            configBuilder.AddJsonFile($"ocelot.{hostBuilderCtx.HostingEnvironment.EnvironmentName}.json",
+                optional: true, reloadOnChange: true);
+        })
+        .ConfigureWebHostDefaults(webhostbuilder =>
+        {
+            webhostbuilder.UseStartup<Startup>();
+        });
+
+
+}
---
+++ ECommerceHub/ApiGateways/Ocelot.Api/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Ocelot.Cache.CacheManager;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+namespace Ocelot.Api;
+
+public class Startup
+{
+
+    public IConfiguration Configuration;
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddOcelot()
+                 .AddCacheManager(o => o.WithDictionaryHandle());
+
+
+
+    }
+    public async void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+
+
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+
+            endpoints.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("hello from ocelot");
+            });
+        });
+
+        await app.UseOcelot();
+
+    }
+
+
+
+
+
+
+}
